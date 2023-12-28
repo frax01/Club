55230c87084edb33256df7c1390e656b38902f77
frax01 <francescomartignoni1@gmail.com>
2023-12-28 20:55:22 +0100

    new pages fm

--- lib/pages/club.dart
+++ lib/pages/club.dart
@@ -10,34 +10,54 @@ class ClubPage extends StatefulWidget {
 }
 
 class _ClubPageState extends State<ClubPage> {
+  var section = 'CLUB';
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    var height = size.height;
+    var width = size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title, style: const TextStyle(color: Colors.white)),
         backgroundColor: const Color.fromARGB(255, 130, 16, 8),
         centerTitle: true,
+        iconTheme: const IconThemeData(color: Colors.white),
       ),
       drawer: Drawer(
+        width: width>700? width/3 : width>400? width/2 : width/1.5,
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
         child: ListView(
           children: [
-            const Image(
-              image: AssetImage('images/Tiber.jpg'),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Flexible(
+                child: Image(
+                  image: const AssetImage('images/photo.jpg'),
+                  width: width/4,
+                  height: height/4,
+                ),
+              ),
             ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Francesco '),
-                Text('Martignoni'),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Francesco ', style: TextStyle(fontSize: width>300? 18 : 14)),
+                  Text('Martignoni', style: TextStyle(fontSize: width>300? 18 : 14)),
+                ],
+              ),
             ),
-            const Text('francescomartignoni1@gmail.com', textAlign: TextAlign.center),          
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0),
+              child: Text('francescomartignoni1@gmail.com', textAlign: TextAlign.center, style: TextStyle(fontSize: width>500? 14 : width>300? 10 : 8)),
+            ),
             DropdownButton(
-              value: 'CLUB',
+              value: section,
               onChanged: (value) {
                 setState(() {
-                  value.toString();
+                  section = value.toString();
                 });
               },
               items: [
@@ -55,7 +75,8 @@ class _ClubPageState extends State<ClubPage> {
               leading: const Icon(
                 Icons.home,
               ),
-              title: const Text('Home'),
+              title: const Text('Home Page'),
+              subtitle: const Text('Club'),
               onTap: () {
                 Navigator.pop(context);
               },
